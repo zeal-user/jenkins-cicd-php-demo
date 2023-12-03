7a70dad9db4a545923a8ead5fc21e14d26edb412
Abdullah Shaikh <128630622+zeal-user@users.noreply.github.com>
2023-12-03 21:08:32 +0530

    Update index.php
    second changed

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Index</title>
  </head>
  <body>
- <?php echo '<p>Hello world from PHP! Declarative pipeline!!Changed</p>'; ?> 
+ <?php echo '<p>Hello world from PHP! Declarative pipeline!!Changed2</p>'; ?> 
  </body>
 </html>
